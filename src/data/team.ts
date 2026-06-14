cb8d972508c70737a98f36a4dbd5e83decc1d2fb
Gjerver <Vetle@gjerver.com>
2026-06-14 15:33:15 +0200

    New team images (.png) + news section solid bg + sine-wave particles with heat effect

--- src/data/team.ts
+++ src/data/team.ts
@@ -18,42 +18,42 @@ export const teamMembers: TeamMember[] = [
   {
     name: "Joar Dale Landa",
     email: "Joar@eldion.no",
-    image: "joar.jpg",
+    image: "joar.png",
     bio: "Sivilingeniør i industriell økonomi (UiS), med fagbrev som automatiker. Har erfaring som automasjonsingeniør hos Siemens Energy.",
     linkedin: "https://www.linkedin.com/in/joar-landa-699664137/",
   },
   {
     name: "Vetle Gjerver",
     email: "Vetle@eldion.no",
-    image: "vetle.jpg",
+    image: "vetle.png",
     bio: "Sivilingeniør i robotteknologi og signalbehandling (UiS), med fagbrev som automatiker. Har erfaring som metering og robotikkingeniør hos Aker Solutions.",
     linkedin: "https://www.linkedin.com/in/vetle-gjerver/",
   },
   {
     name: "Håvard Syslak",
     email: "Haavard@eldion.no",
-    image: "havard.jpg",
+    image: "havard.png",
     bio: "Sivilingeniør i kybernetikk (NTNU), med fagbrev som automatiker. Har erfaring som Embedded & Electronics Engineer hos Blueye Robotics.",
     linkedin: "https://www.linkedin.com/in/h%C3%A5vard-syslak-2956ab176/",
   },
   {
     name: "Harald Domås",
     email: "Harald@eldion.no",
-    image: "harald.jpg",
+    image: "harald.png",
     bio: "Sivilingeniør i robotteknologi og signalbehandling (UiS), med fagbrev som avioniker. Har erfaring som R&D Project Engineer hos ABB Robotics.",
     linkedin: "https://www.linkedin.com/in/harald-dom%C3%A5s-91b324205/",
   },
   {
     name: "Aiken Midttveit",
     email: "Aiken@eldion.no",
-    image: "aiken.jpg",
+    image: "aiken.png",
     bio: "Sivilingeniør i robotteknologi og signalbehandling (UiS), med fagbrev som automatiker. Har erfaring som R&D-elektronikkutvikler hos ABB Robotics.",
     linkedin: "https://www.linkedin.com/in/aiken-midttveit-913b01264/",
   },
   {
     name: "Kristoffer Tufta",
     email: "Kristoffer@eldion.no",
-    image: "kristoffer.jpg",
+    image: "kristoffer.png",
     bio: "Siviløkonomstudent (HVL) innen Master of Business Administration – MBA, Regional Economics and Innovation. Har erfaring som labassistent fra Norsk Hydro.",
     linkedin: "https://www.linkedin.com/in/kristoffertufta/",
   },
@@ -63,7 +63,7 @@ export const mentors: Mentor[] = [
   {
     name: "Kristian Thorsen",
     role: "Akademisk mentor",
-    image: "kristian.jpg",
+    image: "kristian.png",
     title: "Førsteamanuensis ved Universitetet i Stavanger (UiS)",
     description: [
       "Har dyptgående ekspertise innen elektronikkutvikling, robotikk og reguleringsteknikk.",
@@ -73,7 +73,7 @@ export const mentors: Mentor[] = [
   {
     name: "Stig Bruntveit",
     role: "Kommersiell mentor",
-    image: "stig.jpg",
+    image: "stig.png",
     title: "Direktør på Elkem Bjølvefossen",
     description: [
       "Har lang operasjonell ledererfaring, med 14 års bakgrunn fra olje & gass, samt bred erfaring fra aluminiumsindustrien og maritim sektor.",
